460690c8cf3f75fa6213382aa9775707836bde8f
yuudee <85688483+yuudee@users.noreply.github.com>
2022-06-01 10:46:20 +0900

    タイムアウト

--- ABC253/C.py
+++ ABC253/C.py
@@ -4,4 +4,14 @@ Q = int(input())    #クエリ数取得
 
 #各クエリの内容の入力
 que = [input().split() for i in range(Q)]
-print(que)
+
+for q in que:
+    if int(q[0]) == 1:
+        S.append(int(q[1]))
+    elif int(q[0]) == 2:
+        num = min(int(q[2]),S.count(int(q[1])))
+        for j in range(num):
+            S.remove(int(q[1]))
+    elif int(q[0]) == 3:
+        tmp = max(S)-min(S)
+        print(tmp)
