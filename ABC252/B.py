2055a669956d4ce9b354612a258bad5afbb99ec8
yuudee <85688483+yuudee@users.noreply.github.com>
2022-06-03 10:32:24 +0900

    完成版

--- ABC252/B.py
+++ ABC252/B.py
@@ -1,5 +1,16 @@
 N,K = map(int,input().split())
 
-like = [map(int,input().split())]   #それぞれのおいしさが入っている
-dislike_index = [map(int,input().split())] #嫌いな食べ物のインデックス
+like = list(map(int,input().split()))  #それぞれのおいしさが入っている
+dislike_index = list(map(int,input().split())) #嫌いな食べ物のインデックス
 
+max_like = max(like)
+
+flag = 0
+for i in dislike_index:
+    if like[i-1] == max_like:
+        flag = 1
+    
+if flag == 1:
+    print('Yes')
+else:
+    print('No')
